919a86ec7568c991cbe0328ef62ca5acd748e699
nameczz <nameczz@nameczzdeMacBook-Pro.local>
2019-12-11 14:57:34 +0800

    add api

--- src/axios.ts
+++ src/axios.ts
@@ -1,39 +1,13 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
-import Xhr from './xhr'
-import { buildUrl } from './helpers/url'
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeaders } from './helpers/header'
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  // todo
-  processConfig(config)
-  return Xhr(config).then(res => {
-    res.data = transformResponseData(res)
-    return res
-  })
+import { extend } from './helpers/utils'
+import Axios from './core/axios'
+import { AxiosInstance } from './types'
+function createInstance(): AxiosInstance {
+  const context = new Axios()
+  const instance = Axios.prototype.request.bind(context)
+  // 变成混合对象
+  return extend(instance, context)
 }
 
-function processConfig(config: AxiosRequestConfig) {
-  config.url = transformUrl(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
-}
-
-function transformUrl(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildUrl(url, params)
-}
-
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformHeaders(config: AxiosRequestConfig) {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
-function transformResponseData(config: AxiosResponse) {
-  return transformResponse(config.data)
-}
+const axios = createInstance()
 
 export default axios
